6ff727d4159b1bce458bfaf8b2bdfd4c15edef45
ton.yeung <tyeung@auto-master.com>
2014-11-17 14:47:43 -0600

    remove town crier sanity checker
    replaced main method with code to start owin

--- web/Program.cs
+++ web/Program.cs
@@ -7,35 +7,26 @@ using Topshelf;
 
 namespace web
 {
-    public class TownCrier
-    {
-        readonly System.Timers.Timer _timer;
-        public TownCrier()
-        {
-            _timer = new System.Timers.Timer(1000) { AutoReset = true };
-            _timer.Elapsed += (sender, eventArgs) => Console.WriteLine("It is {0} an all is well", DateTime.Now);
-        }
-        public void Start() { _timer.Start(); }
-        public void Stop() { _timer.Stop(); }
-    }
-
     public class Program
     {
         public static void Main()
         {
             HostFactory.Run(x =>
             {
-                x.Service<TownCrier>(s =>
-                {
-                    s.ConstructUsing(name => new TownCrier());
-                    s.WhenStarted(tc => tc.Start());
-                    s.WhenStopped(tc => tc.Stop());
-                });
-                x.RunAsLocalSystem();
+                x.Service<StartUpOwin>
+                (
+                    s =>
+                    {
+                        s.ConstructUsing(() => new StartUpOwin());
+                        s.WhenStarted((service, hostControl) => service.Start(hostControl));
+                        s.WhenStopped((service, hostControl) => service.Stop(hostControl));
+                    }
+                );
 
-                x.SetDescription("Sample Topshelf Host");
-                x.SetDisplayName("Stuff");
-                x.SetServiceName("stuff");
+                x.RunAsLocalSystem();
+                x.SetDescription("owin self host web project");
+                x.SetDisplayName("web-project");
+                x.SetServiceName("web-project");
             });
         }
     }
