ddaa99ad7d96b4430c3fc03d1656b3f4db80d7e3
ton.yeung <tyeung@auto-master.com>
2014-11-17 15:24:28 -0600

    Added an interface and implementing class to test StructureMap

---
+++ web/Concrete/Sample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace web
+{
+    public class Sample : ISample
+    {
+        public string GetDate()
+        {
+            return DateTime.UtcNow.ToShortDateString();
+        }
+    }
+}
---
+++ web/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace web.Controllers
+{
+    public class TestController : ApiController
+    {
+        private readonly ISample sample;
+
+        public TestController(ISample Sample)
+        {
+            this.sample = Sample;
+        }
+
+
+        [HttpGet]
+        public string Get()
+        {
+            return sample.GetDate();
+        }
+    }
+}
