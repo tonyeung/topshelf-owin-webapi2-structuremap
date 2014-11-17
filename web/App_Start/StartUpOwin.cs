ac0a7be71e765cbcd1dbedb379c11e43fd87fa06
ton.yeung <tyeung@auto-master.com>
2014-11-17 15:03:26 -0600

    Added the StartUpOwin class
    Added a section for the BaseUrl
    
    Install-Package structuremap

---
+++ web/App_Start/StartUpOwin.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using Topshelf;
+
+namespace web
+{
+    public class StartUpOwin
+    {
+
+        protected IDisposable OwinWebApp
+        {
+            get;
+            set;
+        }
+
+        public bool Start(HostControl hostControl)
+        {
+            if (OwinWebApp == null)
+            {
+                var baseUrl = ConfigurationManager.ConnectionStrings["BaseUrl"].ToString();
+                var options = new StartOptions(baseUrl);
+
+                OwinWebApp = WebApp.Start
+                (
+                    options,
+                    appBuilder =>
+                    {
+                        new ConfigureOwin().Configure(appBuilder);
+                    }
+                );
+            }
+
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            if (OwinWebApp != null)
+            {
+                OwinWebApp.Dispose();
+                OwinWebApp = null;
+            }
+
+            return true;
+        }
+    }
+}
