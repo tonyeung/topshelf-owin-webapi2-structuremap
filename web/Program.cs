d0dcb59bac17aac22401632f30eccd1bc8b20e5f
ton.yeung <tyeung@auto-master.com>
2014-11-17 14:43:30 -0600

    sanity check, f5 to run for heartbeat

--- web/Program.cs
+++ web/Program.cs
@@ -3,13 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Topshelf;
 
 namespace web
 {
-    class Program
+    public class TownCrier
     {
-        static void Main(string[] args)
+        readonly System.Timers.Timer _timer;
+        public TownCrier()
         {
+            _timer = new System.Timers.Timer(1000) { AutoReset = true };
+            _timer.Elapsed += (sender, eventArgs) => Console.WriteLine("It is {0} an all is well", DateTime.Now);
+        }
+        public void Start() { _timer.Start(); }
+        public void Stop() { _timer.Stop(); }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<TownCrier>(s =>
+                {
+                    s.ConstructUsing(name => new TownCrier());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.RunAsLocalSystem();
+
+                x.SetDescription("Sample Topshelf Host");
+                x.SetDisplayName("Stuff");
+                x.SetServiceName("stuff");
+            });
         }
     }
 }
