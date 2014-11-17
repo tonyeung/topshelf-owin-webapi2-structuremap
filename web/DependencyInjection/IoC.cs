50e56ae950538ece346ae987cadb76430c934507
ton.yeung <tyeung@auto-master.com>
2014-11-17 15:17:42 -0600

    Added the StructureMap registrations
    Added the Activator for WebApi

---
+++ web/DependencyInjection/IoC.cs
@@ -0,0 +1,26 @@
+﻿using StructureMap;
+using StructureMap.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace web
+{
+    public static class IoC
+    {
+        public static IContainer Initialize()
+        {
+            return new Container(x =>
+            {
+                x.Scan(scan =>
+                {
+                    scan.AssembliesFromApplicationBaseDirectory();
+                    scan.ExcludeNamespace("StructureMap");
+                    scan.WithDefaultConventions();
+                });
+            });
+        }
+    }
+}
---
+++ web/DependencyInjection/StructureMapHttpControllerActivator.cs
@@ -0,0 +1,26 @@
+﻿using StructureMap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http.Controllers;
+using System.Web.Http.Dispatcher;
+
+namespace web
+{
+    public class StructureMapHttpControllerActivator : IHttpControllerActivator
+    {
+        IContainer container = null;
+        public StructureMapHttpControllerActivator(IContainer Container)
+        {
+            container = Container;
+        }
+
+        public IHttpController Create(HttpRequestMessage request, HttpControllerDescriptor controllerDescriptor, Type controllerType)
+        {
+            var controller = container.GetInstance(controllerType) as IHttpController;
+            return controller;
+        }
+    }
+}
